96df236dcac1bfa4c0f864dd7e043b1f86897508
Mkylandia <sigmamaale69@gmail.com>
2025-06-12 19:23:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function downloadMKWEB() {
-  window.location.href = "https://www.mediafire.com/file/el6ku8bewa53u1v/MKWEB.zip/file";
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('downloadButton');
+
+    // Der MediaFire-Link, den Sie bereitgestellt haben
+    const mediaFireLink = 'https://www.mediafire.com/file/ye86le93mnhvghg/MKWEB3.zip/file';
+
+    // Event Listener für den Button-Klick
+    if (downloadButton) {
+        downloadButton.addEventListener('click', function() {
+            // Leitet den Benutzer zum MediaFire-Link weiter
+            window.location.href = mediaFireLink;
+        });
+    }
+});
